13879884543f0d9d812a71d5f7c5ee809a9a1445
Dmitry <schedamian@gmail.com>
2024-02-13 23:06:20 +0300

    add adaptive for Footer

--- src/components/Team/Team.tsx
+++ src/components/Team/Team.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import styles from './Team.module.css';
 import H2Title from '../Common/H2Title/H2Title';
 
-import image1 from '../../assets/image/team/1.jpeg';
+import image1 from '../../assets/image/team/1.png';
+import image2 from '../../assets/image/team/2.png';
+import image3 from '../../assets/image/team/3.png';
+import image4 from '../../assets/image/team/4.png';
+import image5 from '../../assets/image/team/5.png';
+import image6 from '../../assets/image/team/6.png';
 import decor from '../../assets/image/team/decor.png';
 import TeamItem from './TeamItem';
 
@@ -21,27 +26,27 @@ const teamItems: TeamItems[] = [
     post: 'Administrator',
   },
   {
-    image: image1,
+    image: image2,
     name: 'Maxim',
     post: 'Administrator',
   },
   {
-    image: image1,
+    image: image3,
     name: 'Maxim',
     post: 'Administrator',
   },
   {
-    image: image1,
+    image: image4,
     name: 'Maxim',
     post: 'Administrator',
   },
   {
-    image: image1,
+    image: image5,
     name: 'Maxim',
     post: 'Administrator',
   },
   {
-    image: image1,
+    image: image6,
     name: 'Maxim',
     post: 'Administrator',
   },
